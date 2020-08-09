e7f29407cc882646c0a4e0c1ccd0eb6bd6d1eedd
Alcides A <aaugusto.kd@gmail.com>
2020-08-09 19:58:36 -0300

    connect to database

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/rello'
+
+db = SQLAlchemy(app)
+
