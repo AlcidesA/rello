43a252776f096ef95c410ef3bb5e2972ad72c29f
Alcides A <aaugusto.kd@gmail.com>
2020-08-12 22:24:58 -0300

    add columns constraints amd auth method

--- api/models/user.py
+++ api/models/user.py
@@ -1,13 +1,16 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50))
+    name = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique = True)
+    password = db.Column(db.String(128), nullable = False)
 
-    def __init__(self, name, email):
+    def __init__(self, name, email, password):
         self.name = name
         self.email = email
+        self.password =  generate_password_hash(password)
 
     def add(new_user):
         db.session.add(new_user)
@@ -17,3 +20,6 @@ class User(db.Model):
             'name': new_user.name,
             'email': new_user.email
         }
+    
+    def authenticate(self, password):
+        return check_password_hash(self.password, password)
